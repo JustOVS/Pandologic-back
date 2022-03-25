289af735b89cee24208bd488314a245979384d5f
JustOVS <57645551+JustOVS@users.noreply.github.com>
2022-03-25 21:06:57 +0300

    Update Jobs.sql

--- JobsDB/Tables/Jobs.sql
+++ JobsDB/Tables/Jobs.sql
@@ -1,6 +1,6 @@
 ﻿create table [dbo].[Jobs]
 (
-	[Id] bigint not null identity(1,1) primary key, 
+    [Id] bigint not null identity(1,1) primary key, 
     [Name] nvarchar(255) not null, 
     [Description] nvarchar(max) null,
     [Opened] datetime2 not null constraint DF_Jobs_Opened default getutcdate(),
